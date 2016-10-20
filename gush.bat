3bab608c7e0468ab2d258ac8e0dce35576b6ff1c
sansna <1185280650@qq.com>
2016-10-20 14:12:23 +0800

    quotes

--- gush.bat
+++ gush.bat
@@ -1,4 +1,16 @@
 echo off
+
+rem throw the first parameter away
+shift
+set params=%1
+:loop
+shift
+if [%1]==[] goto afterloop
+set params=%params% %1
+goto loop
+:afterloop
+rem set params="\"%params%\""
+
 git add -A
-git commit -m %1
+git commit -m %params%
 git push origin master
