6adc573891c61f55050c53561b8552a3884b7360
sansna <1185280650@qq.com>
2016-10-20 14:22:29 +0800

    removing quotes used in gush.bat

--- gush.bat
+++ gush.bat
@@ -2,15 +2,14 @@ echo off
 
 rem throw the first parameter away
 shift
-set params=%1
+set params=%0
 :loop
 shift
-if [%1]==[] goto afterloop
-set params=%params% %1
+if [%0]==[] goto afterloop
+set params=%params% %0
 goto loop
 :afterloop
-rem set params="\"%params%\""
 
 git add -A
-git commit -m %params%
+git commit -m "%params%"
 git push origin master
