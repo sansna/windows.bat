1312abfd83a664892d8462d8141ff32a5aaa3932
sansna <1185280650@qq.com>
2017-10-07 22:35:40 +0800

    now ngroktestport actually working together with ng2 by lnk to startup folder.

---
+++ ng2.bat
@@ -0,0 +1,6 @@
+﻿echo off
+if "%1" == "h" goto begin 
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
+:begin 
+C:\Users\ik\Downloads\Compressed\ngrok-stable-windows-amd64\ngrok.exe tcp 3389
+cls
--- ngroktestport.bat
+++ ngroktestport.bat
@@ -1,9 +1,9 @@
-echo off
+rem echo off
 REM this is the best file so far for learning windows batch.
-if "%1" == "h" goto begin
-mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
-:begin
-curl example.com|grep tcp|cut -d "/" -f 3|cut -d "<" -f 1|sed "s/:/ -p/g"  > testport__.txt
+rem if "%1" == "h" goto begin
+rem mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit
+rem :begin
+curl -s |grep tcp|cut -d "/" -f 3|cut -d "<" -f 1|sed "s/:/ -p/g"  > testport__.txt
 cat testport__.txt|xargs nmap -A -Pn -sS|grep tokucyou|wc -l > testport1__.txt
 REM for /F "delims=" %%i IN ('cat testport1__.txt') DO (set var=%%i)
 set /P var=<testport1__.txt
@@ -13,5 +13,5 @@ del /F testport1__.txt
 REM if nmap test success then wait 10 hours for next nmap test,
 REM otherwise wait 40 sec for me to update the port info retrieved from
 REM https://dashboard.ngrok.com/status.
-if %var% GTR 0 (sleep 36000 & cmd /c ngroktestport.bat h) else (taskkill /F /IM ngrok.exe & cmd /c ng2.bat & sleep 40 & cmd /c ngroktestport.bat h)
+if %var% GTR 0 (sleep 36000 & cmd /c ngroktestport.bat) else (taskkill /F /IM ngrok.exe & cmd /c ng2.bat & sleep 40 & cmd /c ngroktestport.bat)
 cls
