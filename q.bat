9bdb8895f1b72e1127485fbfc8b138c045305d8a
sansna <1185280650@qq.com>
2017-10-19 09:59:22 +0800

    modified qq path

--- q.bat
+++ q.bat
@@ -1,6 +1,6 @@
 echo off
 if "%1" == "h" goto begin 
 mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
-:begin
-"C:\Program Files (x86)\Tencent\QQ\Bin\qq.exe"
+:begin 
+"C:\Users\ik\AppData\Roaming\Tencent\Bin\qq.exe"
 cls
