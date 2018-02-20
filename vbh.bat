dc8bbd5853bd8d7c3e5a9efab4e438d979ed2e06
sansna <1185280650@qq.com>
2018-02-20 21:07:42 +0800

    added vbh for bg vb start

---
+++ vbh.bat
@@ -0,0 +1,6 @@
+echo off
+if "%1" == "h" goto begin 
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h %1",0)(window.close)&&exit 
+:begin
+"C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm %2 --type headless
+cls
