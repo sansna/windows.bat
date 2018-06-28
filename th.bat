935676e801e3d5bf297ec1b4e7537bcc59218190
sansna <1185280650@qq.com>
2018-06-28 13:20:50 +0800

    updates th.bat

--- th.bat
+++ th.bat
@@ -2,5 +2,5 @@ echo off
 if "%1" == "h" goto begin 
 mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
 :begin
-"C:\Program Files (x86)\Thunder Network\Thunder\Program\thunder.exe"
+"C:\Program Files (x86)\Thunder Network\Thunder9\Program\Thunder.exe"
 cls
