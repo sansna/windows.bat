48a04068fece28e576b699eb81f40a5c7738b2b5
sansna <1185280650@qq.com>
2018-10-05 08:32:12 +0800

    adds leproc usage for idm

--- idm.bat
+++ idm.bat
@@ -2,5 +2,5 @@ echo off
 if "%1" == "h" goto begin 
 mshta vbscript:createobject("wscript.shell").run("%~nx0 h",0)(window.close)&&exit 
 :begin
-"C:\Program Files (x86)\idman_lv\IDMan.exe"
+LEProc.exe -runas "8487f82c-18ff-467c-83f5-0e2d3011eee1" "path\to\IDMan.exe"
 cls
