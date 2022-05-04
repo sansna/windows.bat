c15727d52357ba47937bd88a9bdd9e4d5aec9ffb
sansna <1185280650@qq.com>
2022-05-04 13:45:04 +0800

    modify hosts utils

--- hosts.bat
+++ hosts.bat
@@ -1,3 +1,4 @@
-echo off
-@powershell Start-Process hostsa.bat -Verb runas
-cls
+if "%1" == "h" goto begin 
+mshta vbscript:createobject("wscript.shell").run("%~nx0 h %1",0)(window.close)&&exit 
+:begin
+notepad C:\Windows\System32\drivers\etc\hosts
--- hostsa.bat
+++ hostsa.bat
@@ -1,6 +1,1 @@
-echo off
-copy /A C:\WINDOWS\system32\drivers\etc\hosts e:\hosts
-g e:\hosts
-move e:\hosts C:\WINDOWS\system32\drivers\etc\hosts
-del e:\hosts
-cls
+echo 93.46.8.89 %1 >>  C:\Windows\System32\drivers\etc\hosts
